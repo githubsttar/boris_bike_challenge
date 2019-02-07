b0e7f518fa65c88d33852cd5833e27e849f19770
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2019-02-07 18:11:56 +0000

    guard condition added to DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,25 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
+
+  describe '#release_bike' do
+    it 'raises error when there are no bikes available' do
+      bike = Bike.new
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  # it 'releases working bikes' do
+  #   bike = subject.release_bike
+  #   expect(bike).to be_working
+  # end
 
   it 'docks a bike' do
     bike = Bike.new
